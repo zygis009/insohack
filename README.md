c1e39ff5401f38b7e88e41cbd89496105075900f
zygis <liutkus@student.tudelft.nl>
2024-10-16 13:25:23 +0200

    Remove comment block

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # insohack
 Repository for the Insocial AI hackathon
+
+## Formatting
+### Anthropic message format:
+```
+Message(
+    id='msg_12345abcd...',
+    content=[TextBlock(
+    text="Hello! I'm happy to hear that you just bought (...)",
+    type='text')],
+    model='claude-3-haiku-20240307',
+    role='assistant',
+    stop_reason='end_turn',
+    stop_sequence=None,
+    type='message',
+    usage=Usage(input_tokens=106, output_tokens=49)
+)
+```
--- insohack/llm_access.py
+++ insohack/llm_access.py
@@ -33,21 +33,7 @@ def send_conversation_message(client, content):
         system=prompt,
         messages=conversation,
     )
-    # Message format:
-    # Message(
-    #   id='msg_015MXD4NyE5DMirqWRv8VHFs',
-    #   content=[TextBlock(
-    #       text="Hello! I'm happy to hear that you just bought (...)",
-    #       type='text')],
-    #   model='claude-3-haiku-20240307',
-    #   role='assistant',
-    #   stop_reason='end_turn',
-    #   stop_sequence=None,
-    #   type='message',
-    #   usage=Usage(input_tokens=106, output_tokens=49)
-    # )
 
-    # Note: template (with assumptions) for parsing response
     conversation.append({"role": "assistant", "content": message.content[0].text})
     return message
 
