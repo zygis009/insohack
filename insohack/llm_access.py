4d5542c8ac8cf2d8e0695c05a7944a76f82e3818
zygis <liutkus@student.tudelft.nl>
2024-10-16 12:29:48 +0200

    Update LLM template

--- insohack/llm_access.py
+++ insohack/llm_access.py
@@ -4,6 +4,9 @@ import anthropic
 with open ('prompt.txt', 'r') as file:
     prompt = file.read()
 
+conversation = [{"role": "system", "content": prompt}]
+
+
 def create_client():
     key = os.getenv('LLM_API_KEY')
     client = anthropic.Client(api_key=key)
@@ -11,12 +14,27 @@ def create_client():
     return client
 
 
-def send_user_message(client, content):
+def send_conversation_message(client, content):
+    conversation.append({"role": "user", "content": content})
     message = client.messages.create(
-    model="claude-3-haiku-20240307",
-    max_tokens=1024,
-    messages=[
-        {"role": "system", "content": prompt},
-        {"role": "user", "content": content}
-    ]
-)
+        model="claude-3-haiku-20240307",
+        max_tokens=1024,
+        messages=conversation,
+    )
+    # Note: template (with assumptions) for parsing response
+    conversation.append({"role": "assistant", "content": message["content"][0]["text"]})
+    return message
+
+
+def send_single_message(client, content):
+    message = client.messages.create(
+        model="claude-3-haiku-20240307",
+        max_tokens=1024,
+        messages=[{"role": "system", "content": prompt}, {"role": "user", "content": content}],
+    )
+    return message
+
+
+def flush_conversation():
+    global conversation
+    conversation = [{"role": "system", "content": prompt}]
